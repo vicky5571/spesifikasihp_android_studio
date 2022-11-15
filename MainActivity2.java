a51f5d57108314eff1a86f0ad46b357832c9b37e
vicky5571 <111561469+vicky5571@users.noreply.github.com>
2022-11-15 20:39:30 +0700

    Add files via upload

---
+++ MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.profil_dr;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
